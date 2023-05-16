df6cfb3218e1f31222c4462ff3bee20b2e5c2369
fonatabaJulia <julia.theresia@student.umn.ac.id>
2023-05-16 20:36:21 +0700

    Attaching observer

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -33,9 +33,6 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 class GameFragment : Fragment() {
 
     private val viewModel: GameViewModel by viewModels()
-//    private var score = 0
-//    private var currentWordCount = 0
-//    private var currentScrambledWord = "test"
 
 
     // Binding object instance with access to the views in the game_fragment.xml layout
@@ -51,9 +48,10 @@ class GameFragment : Fragment() {
     ): View {
         binding = GameFragmentBinding.inflate(inflater, container, false)
         Log.d("GameFragment", "GameFragment created/re-created!")
-
-        Log.d("GameFragment", "Word: ${viewModel.currentScrambledWord} " +
-                "Score: ${viewModel.score} WordCount: ${viewModel.currentWordCount}")
+        Log.d(
+            "GameFragment", "Word: ${viewModel.currentScrambledWord} " +
+                    "Score: ${viewModel.score} WordCount: ${viewModel.currentWordCount}"
+        )
         return binding.root
     }
 
@@ -64,11 +62,27 @@ class GameFragment : Fragment() {
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
         // Update the UI
-        updateNextWordOnScreen()
-        binding.score.text = getString(R.string.score, 0)
-        binding.wordCount.text = getString(
-                R.string.word_count, 0, MAX_NO_OF_WORDS)
+
+
+        viewModel.currentScrambledWord.observe(viewLifecycleOwner,
+            { newWord ->
+                binding.textViewUnscrambledWord.text = newWord
+
+            })
+
+        viewModel.score.observe(viewLifecycleOwner,
+            { newScore ->
+                binding.score.text = getString(R.string.score, newScore)
+            })
+
+        viewModel.currentWordCount.observe(viewLifecycleOwner,
+            { newWordCount ->
+                binding.wordCount.text =
+                    getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
+            })
+
     }
+
 
     /*
     * Checks the user's word, and updates the score accordingly.
@@ -76,16 +90,10 @@ class GameFragment : Fragment() {
     */
     private fun onSubmitWord() {
         val playerWord = binding.textInputEditText.text.toString()
-//        if (viewModel.nextWord()) {
-//            updateNextWordOnScreen()
-//        } else {
-//            showFinalScoreDialog()
-//        }
+//
         if (viewModel.isUserWordCorrect(playerWord)) {
             setErrorTextField(false)
-            if (viewModel.nextWord()) {
-                updateNextWordOnScreen()
-            } else {
+            if (!viewModel.nextWord()) {
                 showFinalScoreDialog()
             }
         } else {
@@ -94,7 +102,8 @@ class GameFragment : Fragment() {
 
 
 
-    }
+
+}
 
     /*
      * Skips the current word without changing the score.
@@ -103,7 +112,7 @@ class GameFragment : Fragment() {
     private fun onSkipWord() {
         if (viewModel.nextWord()) {
             setErrorTextField(false)
-            updateNextWordOnScreen()
+
         } else {
             showFinalScoreDialog()
         }
@@ -122,7 +131,7 @@ class GameFragment : Fragment() {
     private fun showFinalScoreDialog() {
         MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.congratulations))
-            .setMessage(getString(R.string.you_scored, viewModel.score))
+            .setMessage(getString(R.string.you_scored, viewModel.score.value))
             .setCancelable(false)
             .setNegativeButton(getString(R.string.exit)) { _, _ ->
                 exitGame()
@@ -140,7 +149,7 @@ class GameFragment : Fragment() {
     private fun restartGame() {
         viewModel.reinitializeData()
         setErrorTextField(false)
-        updateNextWordOnScreen()
+
     }
 
     /*
@@ -167,9 +176,7 @@ class GameFragment : Fragment() {
     /*
      * Displays the next scrambled word on screen.
      */
-    private fun updateNextWordOnScreen() {
-        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
-    }
+
 
     override fun onDetach() {
         super.onDetach()
--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,64 +1,147 @@
 package com.example.android.unscramble.ui.game
 
-import android.util.Log
-import androidx.lifecycle.ViewModel
+//import android.util.Log
+//import androidx.lifecycle.ViewModel
+//
+//class GameViewModel : ViewModel(){
+//    private var wordsList: MutableList<String> = mutableListOf()
+//    private lateinit var currentWord: String
+//
+//
+//    private fun getNextWord() {
+//        currentWord = allWordsList.random()
+//        val tempWord = currentWord.toCharArray()
+//        tempWord.shuffle()
+//
+//        while (String(tempWord).equals(currentWord, false)) {
+//            tempWord.shuffle()
+//        }
+//        if (wordsList.contains(currentWord)) {
+//            getNextWord()
+//        } else {
+//            _currentScrambledWord = String(tempWord)
+//            ++_currentWordCount
+//            wordsList.add(currentWord)
+//        }
+//
+//    }
+//
+//    init {
+//        Log.d("GameFragment", "GameViewModel created!")
+//        getNextWord()
+//    }
+//
+//    private var _score = 0
+//    val score: Int
+//        get() = _score
+//    private var _currentWordCount = 0
+//    val currentWordCount: Int
+//        get() = _currentWordCount
+//   // private var _currentScrambledWord = "test"
+//    private lateinit var _currentScrambledWord: String
+//    val currentScrambledWord: String
+//        get() = _currentScrambledWord
+//
+//
+//    override fun onCleared() {
+//        super.onCleared()
+//        Log.d("GameFragment", "GameViewModel destroyed!")
+//    }
+//
+//    fun nextWord(): Boolean {
+//        return if (_currentWordCount < MAX_NO_OF_WORDS) {
+//            getNextWord()
+//            true
+//        } else false
+//    }
+//
+//    private fun increaseScore() {
+//        _score += SCORE_INCREASE
+//    }
+//
+//    fun isUserWordCorrect(playerWord: String): Boolean {
+//        if (playerWord.equals(currentWord, true)) {
+//            increaseScore()
+//            return true
+//        }
+//        return false
+//    }
+//
+//    fun reinitializeData() {
+//        _score = 0
+//        _currentWordCount = 0
+//        wordsList.clear()
+//        getNextWord()
+//    }
+//
+//
+//}
 
+
+//--------
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+/**
+ * ViewModel containing the app data and methods to process the data
+ */
 class GameViewModel : ViewModel(){
+    private val _score = MutableLiveData(0)
+    val score: LiveData<Int>
+        get() = _score
+    private val _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int>
+        get() = _currentWordCount
+
+    private val _currentScrambledWord = MutableLiveData<String>()
+    val currentScrambledWord: LiveData<String>
+        get() = _currentScrambledWord
+    // List of words used in the game
     private var wordsList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
-
-
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+    /*
+    * Updates currentWord and currentScrambledWord with the next word.
+    */
     private fun getNextWord() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
         tempWord.shuffle()
-
         while (String(tempWord).equals(currentWord, false)) {
             tempWord.shuffle()
         }
         if (wordsList.contains(currentWord)) {
             getNextWord()
         } else {
-            _currentScrambledWord = String(tempWord)
-            ++_currentWordCount
+            _currentScrambledWord.value = String(tempWord)
+            _currentWordCount.value = (_currentWordCount.value)?.inc()
             wordsList.add(currentWord)
+
         }
-
     }
-
-    init {
-        Log.d("GameFragment", "GameViewModel created!")
+    /*
+    * Re-initializes the game data to restart the game.
+    */
+    fun reinitializeData() {
+        _score.value = 0
+        _currentWordCount.value = 0
+        wordsList.clear()
         getNextWord()
     }
-
-    private var _score = 0
-    val score: Int
-        get() = _score
-    private var _currentWordCount = 0
-    val currentWordCount: Int
-        get() = _currentWordCount
-   // private var _currentScrambledWord = "test"
-    private lateinit var _currentScrambledWord: String
-    val currentScrambledWord: String
-        get() = _currentScrambledWord
-
-
-    override fun onCleared() {
-        super.onCleared()
-        Log.d("GameFragment", "GameViewModel destroyed!")
-    }
-
-    fun nextWord(): Boolean {
-        return if (_currentWordCount < MAX_NO_OF_WORDS) {
-            getNextWord()
-            true
-        } else false
-    }
-
+    /*
+    * Increases the game score if the player's word is correct.
+    */
     private fun increaseScore() {
-        _score += SCORE_INCREASE
+        _score.value =  (_score.value)?.plus(SCORE_INCREASE)
     }
-
     fun isUserWordCorrect(playerWord: String): Boolean {
         if (playerWord.equals(currentWord, true)) {
             increaseScore()
@@ -66,13 +149,13 @@ class GameViewModel : ViewModel(){
         }
         return false
     }
-
-    fun reinitializeData() {
-        _score = 0
-        _currentWordCount = 0
-        wordsList.clear()
-        getNextWord()
+    /*
+    * Returns true if the current word count is less than MAX_NO_OF_WORDS
+    */
+    fun nextWord(): Boolean {
+        return if (_currentWordCount.value !! <  MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
     }
-
-
 }
